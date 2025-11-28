c2ec9755bfdc41366adf9fa6d7609a59144f2812
yankarlosb <balujayankarlos7@gmail.com>
2025-11-28 14:24:48 -0500

    Added view balance functionality

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,7 +4,5 @@ use web_project::run;
 
 #[launch]
 async fn rocket() -> _ {
-    // Removed bcrypt hash on startup - this was slow and unnecessary for production
-    // To generate a password hash, use: cargo run --example hash_password
     run().await
 }
